ae223cfa3ef0acf215f38f32582562d7ff1bab85
Martijn Schiferli <martijn@schiferli.com>
2017-12-23 20:17:22 +1100

    some rework

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/gob"
 	"encoding/json"
 	"fmt"
@@ -12,7 +15,10 @@ import (
 	"time"
 
 	"cloud.google.com/go/datastore"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/jasonlvhit/gocron"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type DataLoad struct {
@@ -44,8 +50,142 @@ var projectID = os.Getenv("projectID")
 var url = fmt.Sprintf("http://%v/api/meters/aggregates", os.Getenv("PW2IP"))
 var interval uint64 = 5
 
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
+const (
+	// For simplicity these files are in the same folder as the app binary.
+	// You shouldn't do this in production.
+	privKeyPath = "rsa_private.pem"
+	pubKeyPath  = "rsa_cert.pem"
+)
+
+var (
+	verifyKey *rsa.PublicKey
+	signKey   *rsa.PrivateKey
+)
+
+func fatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func initKeys() {
+	signBytes, err := ioutil.ReadFile(privKeyPath)
+	fatal(err)
+
+	signKey, err = jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	fatal(err)
+
+	verifyBytes, err := ioutil.ReadFile(pubKeyPath)
+	fatal(err)
+
+	verifyKey, err = jwt.ParseRSAPublicKeyFromPEM(verifyBytes)
+	fatal(err)
+}
+
+func NewTlsConfig() *tls.Config {
+
+	// Import client certificate/key pair
+	cert, err := tls.LoadX509KeyPair("samplecerts/client-crt.pem", "samplecerts/client-key.pem")
+	if err != nil {
+		panic(err)
+	}
+
+	// Just to print out the client certificate..
+	cert.Leaf, err = x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		panic(err)
+	}
+
+	// Create tls.Config with desired tls properties
+	return &tls.Config{
+		// RootCAs = certs used to verify server cert.
+		RootCAs: nil,
+		// ClientAuth = whether to request cert from server.
+		// Since the server is set up for SSL, this happens
+		// anyways.
+		ClientAuth: tls.NoClientCert,
+		// ClientCAs = certs used to validate client cert.
+		ClientCAs: nil,
+		// InsecureSkipVerify = verify that cert contents
+		// match server. IP matches what is in cert etc.
+		InsecureSkipVerify: true,
+		// Certificates = list of certs client sends to server.
+		Certificates: []tls.Certificate{cert},
+	}
+}
+
 func main() {
+	initKeys()
+
+	claims := &jwt.StandardClaims{
+		IssuedAt:  time.Now().Unix(),
+		ExpiresAt: time.Now().Unix() + 3600,
+		Audience:  "powerchat-187002",
+	}
+
+	tokenb := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	ss, _ := tokenb.SignedString(signKey)
+
+	fmt.Println(ss)
+	//create a ClientOptions struct setting the broker address, clientid, turn
+	//off trace output and set the default message handler
+
+	tlsconfig := NewTlsConfig()
+
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("ssl://mqtt.googleapis.com:8883")
+	opts.SetTLSConfig(tlsconfig)
+	mqttClientID := fmt.Sprintf("projects/%v/locations/asia-east1/registries/home/devices/dev1", projectID)
+
+	fmt.Println(mqttClientID)
+
+	opts.SetClientID(mqttClientID)
+	opts.SetUsername("unused")
+	opts.SetProtocolVersion(4)
+	opts.SetPassword(ss)
+
+	opts.SetDefaultPublishHandler(f)
+
+	//create and start a client using the above ClientOptions
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
+	//at a maximum qos of zero, wait for the receipt to confirm the subscription
+	if token := c.Subscribe("projects/powerchat-187002/topics/actions", 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
+	//from the server after sending each message
+	for i := 0; i < 5; i++ {
+		text := fmt.Sprintf("this is msg #%d!", i)
+		token := c.Publish("projects/powerchat-187002/topics/devicetelemetry", 0, false, text)
+		token.Wait()
+	}
+
+	time.Sleep(3 * time.Second)
+
+	//unsubscribe from /go-mqtt/sample
+	if token := c.Unsubscribe("projects/powerchat-187002/topics/actions"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	c.Disconnect(250)
+}
+
+func main2() {
 	gob.Register(BatteryOutputData{})
+	gob.Register(DataLoad{})
 	gocron.Every(interval).Seconds().Do(task)
 	<-gocron.Start()
 }
